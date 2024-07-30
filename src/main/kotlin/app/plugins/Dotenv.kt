1957dc7dc3aceaded8905a054e388750529b9b7f
Marc Allen Nanong <marcallen.nanong11@gmail.com>
2024-07-30 20:02:34 +0800

    On going development for ci/cd

--- src/main/kotlin/app/plugins/Dotenv.kt
+++ src/main/kotlin/app/plugins/Dotenv.kt
@@ -2,12 +2,20 @@ package app.plugins
 
 import app.utils.Breadcrumb
 import io.github.cdimascio.dotenv.Dotenv
+import io.github.cdimascio.dotenv.DotenvException
 
 lateinit var dotenv: Dotenv
 fun initializeDotenv(breadcrumb: Breadcrumb) {
     breadcrumb.log("Dotenv initializing...")
-    dotenv = Dotenv
-        .configure()
-        .directory("app/resources/.env") // Specify the correct directory
-        .load()
+    dotenv = try {
+        Dotenv
+            .configure()
+            .directory("app/resources/.env")
+            .load()
+    } catch (e: DotenvException) {
+        Dotenv
+            .configure()
+            .directory("src/main/resources/.env")
+            .load()
+    }
 }
